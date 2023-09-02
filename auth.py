fe182413592461ec94b6845a92546dba4a93c886
João Victor Guedes <vcitorsguedes@hotmail.com>
2023-09-02 20:29:35 -0300

    refactor: add api connection

--- auth.py
+++ auth.py
@@ -2,3 +2,8 @@ from parameters import *
 import tweepy
 
 client = tweepy.Client(consumer_key=API_KEY, consumer_secret=API_KEY_SECRET, access_token=ACCESS_TOKEN, access_token_secret=ACCESS_TOKEN_SECRET)
+
+auth = tweepy.OAuthHandler(API_KEY, API_KEY_SECRET)
+auth.set_access_token(ACCESS_TOKEN, ACCESS_TOKEN_SECRET)
+
+api = tweepy.API(auth)
