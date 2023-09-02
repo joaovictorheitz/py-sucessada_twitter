3d11d03f07d1dedf13e5f02a971c78db1e428e24
João Victor Guedes <vcitorsguedes@hotmail.com>
2023-09-02 20:03:00 -0300

    refactor: removed function line from auth.py

--- auth.py
+++ auth.py
@@ -8,5 +8,3 @@ auth = tweepy.OAuth1UserHandler(
 api = tweepy.API(auth)
 
 client = tweepy.Client(consumer_key=API_KEY, consumer_secret=API_KEY_SECRET, access_token=ACCESS_TOKEN, access_token_secret=ACCESS_TOKEN_SECRET)
-
-client.get_tweet()
